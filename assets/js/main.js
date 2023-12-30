57b5df3e52ee408fb5f44d29fc959afd7f5dda93
sonicalink <155162612+sonicalink@users.noreply.github.com>
2023-12-30 09:45:07 +0800

    Removed parallax effect
    For better compatibility

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,16 +53,6 @@ jQuery(document).ready(function($) {
         
     });
     
-    
-    // INIT PARALLAX PLUGIN
-    $(".hero .background-content.parallax-on").parallax({
-        scalarX: 24,
-        scalarY: 15,
-        frictionX: 0.1,
-        frictionY: 0.1,
-    });
-    
-    
     // OPEN POPUP SEQUENCE
     $(".open-popup").click(function(){
         
